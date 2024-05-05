878ca704bcba8aef6d767df8d37553143f41954f
dixma94 <32520039+dixma94@users.noreply.github.com>
2024-05-05 13:15:58 +0600

    Create second Example

---
+++ Assets/Example2/Scripts/LevelDataProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LevelDataProvider : MonoBehaviour, IDataProvider<LevelData>
+{
+    public List<LevelData> data = new List<LevelData>();
+    public IEnumerable<LevelData> GetData()
+    {
+        return data;
+    }
+
+    public LevelData GetData(int id)
+    {
+        LevelData dataItem = data.Where(x => x.Id == id).First();
+        return dataItem;
+    }
+}
---
+++ Assets/Example2/Scripts/LevelSelectedView.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectedView : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI Name;
+    [SerializeField] TextMeshProUGUI Description;
+    [SerializeField] Image image;
+
+    private void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show(LevelData levelData)
+    {
+        gameObject.SetActive(true);
+        Name.text = levelData.Name;
+        Description.text =levelData.Description;
+        image.sprite = levelData.Sprite;
+    }
+}
